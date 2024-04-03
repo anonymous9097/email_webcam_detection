ce44314df11c263f2a8d33a4b027623ef3055e95
divyangkhare <divyang.khare01@gmail.com>
2024-04-03 12:10:13 +0530

    implemented threading to fix video lag.

--- emailing.py
+++ emailing.py
@@ -8,6 +8,7 @@ RECEIVER = "appd6087.datia@gmail.com"
 
 
 def send_email(image_path):
+    print("send_email function started")
     email_message = EmailMessage()
     email_message["Subject"] = "New customer showed up!"
     email_message.set_content("Hey, we just saw a new customer!")
@@ -16,12 +17,13 @@ def send_email(image_path):
         content = file.read()
     email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
 
-    gmail = smtplib.SMTP("smtp@gmail.com", 587)
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
     gmail.ehlo()
     gmail.starttls()
     gmail.login(SENDER, PASSWORD)
     gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
     gmail.quit()
+    print("send_email function ended")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 import cv2
 import time
 from emailing import send_email
+from threading import Thread
 import glob
 
 video = cv2.VideoCapture(0)
@@ -9,6 +11,15 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+
+def clean_folder():
+    print("clean_folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("clean_folder function ended")
+
 
 while True:
     status = 0
@@ -44,7 +55,12 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder())
+        clean_thread.daemon = True
+
+        email_thread.start()
 
     print(status_list)
 
@@ -56,3 +72,5 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
